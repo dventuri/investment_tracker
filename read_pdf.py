0bbe19fa1534c6eff89996078e1f6c34014730a2
Diego N. Venturi <diegoventuri@gmail.com>
2022-10-18 00:02:47 -0300

    teste ler pdf - loop em mais de uma operacao e criacao de objetos operacao

--- read_pdf.py
+++ read_pdf.py
@@ -3,6 +3,7 @@ from decimal import Decimal
 from PyPDF2 import PdfReader
 from click import FileError
 from numpy import quantile
+from main import *
 
 fname = "000991087_SPOT_FORWARD_20221014_N002608650.pdf"
 
@@ -27,11 +28,35 @@ data_lida = text.splitlines(True)[3].split()[0].split(sep="/")
 data = date(int(data_lida[-1]), int(data_lida[-2]), int(data_lida[0]))
 
 #Loop enquanto linha começar com 1-BOVESPA?
-linha = text.splitlines(True)[26].split()
-kind = "compra" if linha[1] == "CVISTA" else "venda"
-papel = linha[2]
-quantidade = int(linha[3])
-preco_unitario = Decimal(linha[4].replace(",", "."))
+lista_de_operacoes = []
+for linha in text.splitlines(True)[26:]:
+    
+    linha_split = linha.split()
+
+    if (linha_split[0] != "1-BOVESPA"):
+        break
+
+    papel = Papel(linha_split[2])
+
+    quantidade = int(linha_split[3])
+
+    preco_unitario = Decimal(linha_split[4].replace(",", "."))
+
+    if (linha_split[1] == "CVISTA"):
+        lista_de_operacoes.append(
+            Compra(
+                id_nota, data, papel, quantidade, preco_unitario,
+                Decimal(0), Decimal(0), Decimal(0), Decimal(0)
+            )
+        )
+    else:
+        lista_de_operacoes.append(
+            Venda(
+                id_nota, data, papel, quantidade, preco_unitario,
+                Decimal(0), Decimal(0), Decimal(0), Decimal(0)
+            )
+        )
+
 
 #nova linha será o número da última acima + x, no caso atual:
 liquidacao = Decimal(text.splitlines(True)[38].split()[0].replace(",", "."))
